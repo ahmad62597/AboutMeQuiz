132b860d0c6f9c09189e64c0704414c66ae50a9b
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-08 12:34:39 -0800

    questions and answer array

--- AboutMeQuiz/Program.cs
+++ AboutMeQuiz/Program.cs
@@ -5,9 +5,40 @@ namespace AboutMeQuiz
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //welcome message
             Console.WriteLine("Welcome to my About Me Quiz! How well do you know me?");
+            Console.ReadKey();
+
+            //array for my questions
+            string[] questions =
+            {
+                "Where was I born?",
+                "What is my favorite animal?",
+                "How old am I?",
+                "What is my favorite book?",
+                "True or False: :I know 4 different languages"
+
+            };
+
+            //array for my answers in order of the questions
+            string[] answers =
+                
+             {
+           
+                "Karachi",
+                "Tiger",
+                "22",
+                "Lord of the Rings",
+                "True"
+                };
+
+            //stretch goal for correct answer counter
+            int totalscore = 0;
+
+
 
         }
+
+      
     }
 }
