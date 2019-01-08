cf4c5c293b11bfac899b9484068ed55204bacc9c
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-08 11:41:52 -0800

    initial

---
+++ AboutMeQuiz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AboutMeQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my About Me Quiz! How well do you know me?");
+
+        }
+    }
+}
